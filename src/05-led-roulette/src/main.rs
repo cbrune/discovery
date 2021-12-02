e86338bda3956bcedf3fca8165935cab056a0164
Curt Brune <curt@brune.net>
2021-12-01 21:03:03 -0800

    chap5: spinning LEDs
    implemented the spinning LEDs
    
    Signed-off-by: Curt Brune <curt@brune.net>

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,31 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let tick = 50_u16;
+    let mut iter = 0;
+
+    let mut on_mask = [0_u16; 16];
+    on_mask[0] = 0xC001;
+
+    for i in 1..15 {
+        on_mask[i] = 0x3 << (i - 1);
+    }
+
+    loop {
+        for led in 0..8 {
+            if (on_mask[led] & (1 << iter)) != 0 {
+                leds[led].on().ok();
+            } else {
+                leds[led].off().ok();
+            }
+        }
+        delay.delay_ms(tick);
+        iter = (iter + 1) % 8;
+    }
 }
